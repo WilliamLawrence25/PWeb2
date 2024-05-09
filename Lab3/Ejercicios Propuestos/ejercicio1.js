3a972ca488ccd657809b4d032f395c1a7fe70b2a
wchoquehuancab <wchoquehuancab@unsa.edu.pe>
2024-05-09 00:57:36 -0500

    Ejercicio 1 y 2 resueltos

---
+++ Lab3/Ejercicios Propuestos/ejercicio1.js
@@ -0,0 +1,6 @@
+
+function diaSemana(numeroDia){
+    let semana = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
+    return semana[numeroDia];
+}
+console.log(diaSemana(1));
---
+++ Lab3/Ejercicios Propuestos/ejercicio2.js
@@ -0,0 +1,6 @@
+
+document.getElementById("botonInvertir").addEventListener("click", function(){
+    let texto = document.getElementById("texto").value;
+    let texInvertido = texto.split("").reverse().join("");
+    document.getElementById("resultado").innerText = texInvertido;
+});

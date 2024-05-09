20b95f2a94fd2a8ca7b39b5956d1270e0a196eb6
wchoquehuancab <wchoquehuancab@unsa.edu.pe>
2024-05-09 08:14:09 -0500

    ejercicio 4 completado

--- Lab3/Ejercicios Propuestos/ejercicio4.js
+++ Lab3/Ejercicios Propuestos/ejercicio4.js
@@ -1,4 +1,7 @@
 function codigoMeet() {
-    let link = document.getElementById("obtenerLink");
-
+    let link = document.getElementById("URL").value;
+    let codigo = link.replace(/-/g, '');
+    let codigoFinal = codigo.substring(codigo.lastIndexOf('/') + 1);
+    document.getElementById("resultado").innerText = "Codigo de sesion: " + codigoFinal;
 }
+

6be5d42c25df051eaf973b483ff9812a254ba4c3
wchoquehuancab <wchoquehuancab@unsa.edu.pe>
2024-05-09 09:27:26 -0500

    ejercicio 5: completado

--- Lab3/Ejercicios Propuestos/ejercicio5.js
+++ Lab3/Ejercicios Propuestos/ejercicio5.js
@@ -1,19 +1,16 @@
-document.getElementByI("formulario").addEventListener("submit", function(event) {
-    
+document.getElementById("formulario").addEventListener("submit", function(event) {
+    event.preventDefault();
+
     let cantidadValores = parseInt(document.getElementById("cantidadValores").value);
     var tablaHTML = "<table><tr><th>Valores</th></tr>";
-    for (let i = 0; i < cantidadValores; i++){
+    let suma = 0;
+    for (let i = 0; i < cantidadValores; i++) {
         let numero = Math.floor(Math.random() * 100) + 1;
-            tablaHTML += "<tr><th>" + numero + "</th></tr>";
+        tablaHTML += "<tr><th>" + numero + "</th></tr>";
+        suma += numero;
     }
     tablaHTML += "</table>";
-    document.getElementById("tablaContainer").innerHTML = tablaHTML; 
+    document.getElementById("tablaContainer").innerHTML = tablaHTML;
 
-    let valores = document.querySelectorAll("#tablaContainer table td");
-    let suma = 0;
-    for (let i = 0; i < valores.length; i++){
-        suma += parseInt(valores[i]);
-    }
     document.getElementById("resultado").innerText = "La suma total es: " + suma;
 });
-

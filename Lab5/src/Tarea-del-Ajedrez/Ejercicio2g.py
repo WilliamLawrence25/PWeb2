9789c1b7037b522c54294612921cee6eda36e174
wchoquehuancab <wchoquehuancab@unsa.edu.pe>
2024-05-23 13:57:15 -0500

    Ejercicio2g: se simplifico un poco el codigo

--- Lab5/src/Tarea-del-Ajedrez/Ejercicio2g.py
+++ Lab5/src/Tarea-del-Ajedrez/Ejercicio2g.py
@@ -3,11 +3,10 @@ from chessPictures import *
 
 type1 = (square.join(square.negative()).horizontalRepeat(4))
 type2 = (square.negative().join(square).horizontalRepeat(4))
-type3 = type1.up(type2).verticalRepeat(2)
 
 chess1s = type2.under(rock.join(knight).join(bishop).join(queen).join(king).join(bishop).join(knight).join(rock)).negative()
 chess2s = type1.under(pawn.horizontalRepeat(8)).negative()
 
-table = chess1s.up(chess2s).up(type3).up(chess2s.negative()).up(chess1s.negative())
+table = chess1s.up(chess2s.up(type1.up(type2).verticalRepeat(2))).up(chess2s.negative()).up(chess1s.negative())
 
 draw (table)

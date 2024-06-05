526c488c4cf0be7419343448480421b495779639
wchoquehuancab <wchoquehuancab@unsa.edu.pe>
2024-06-04 22:21:28 -0500

    Modificacion de los html y css para mas estetica

--- lab07/destinos/models.py
+++ lab07/destinos/models.py
@@ -7,7 +7,7 @@ class DestinosTuristicos(models.Model):
     nombreCiudad = models.CharField(max_length=100)
     descripcionCiudad = models.TextField()
     imagenCiudad = models.ImageField(upload_to='images/')
-    precioTour = models.DecimalField(max_digits=2, decimal_places=2)
+    precioTour = models.DecimalField(max_digits=10, decimal_places=2)
     ofertaTour = models.BooleanField(default=False)
 
     def __str__(self) -> str:

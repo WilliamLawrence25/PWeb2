aafcf5e6e231d7b57855dfc885dbe9a316476ecb
wchoquehuancab <wchoquehuancab@unsa.edu.pe>
2024-06-11 11:28:55 -0500

    Añadiendo trabajo base para realizar las actividades

---
+++ Lab8/destinos/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Canada se multicultural
+
+# Create your models here.
+class DestinosTuristicos(models.Model):
+    nombreCiudad = models.CharField(max_length=100)
+    descripcionCiudad = models.TextField()
+    imagenCiudad = models.ImageField(upload_to='images/')
+    precioTour = models.DecimalField(max_digits=10, decimal_places=2)
+    ofertaTour = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.nombre

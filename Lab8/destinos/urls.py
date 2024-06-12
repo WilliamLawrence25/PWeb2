ebb607756a66a9bb9dc12b63e94df16cf3cdbc49
wchoquehuancab <wchoquehuancab@unsa.edu.pe>
2024-06-12 00:54:16 -0500

    Se agregaron las nuevas URLs

--- Lab8/destinos/urls.py
+++ Lab8/destinos/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('añadir/', views.añadir_destino, name='añadir_destino'),
     path('modificar/<int:id>/', views.modificar_destino, name='modificar_destino'),
     path('eliminar/<int:id>/', views.eliminar_destino, name='eliminar_destino'),
+    path('comentario/añadir/<int:destino_id>/', views.añadir_comentario, name='añadir_comentario'),
+    path('categoria/añadir/', views.añadir_categoria, name='añadir_categoria'), 
+    path('categorias/', views.listar_categorias, name='listar_categorias'),
 ]

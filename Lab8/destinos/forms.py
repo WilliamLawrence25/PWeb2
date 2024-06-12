af8f3b4668990e503bce1665d780b1bfa1a7ff0d
wchoquehuancab <wchoquehuancab@unsa.edu.pe>
2024-06-12 00:32:48 -0500

    Nuevos formularios agregados para los comentarios y categorias

--- Lab8/destinos/forms.py
+++ Lab8/destinos/forms.py
@@ -1,7 +1,17 @@
 from django import forms
-from .models import DestinosTuristicos
+from .models import DestinosTuristicos, Comentario, Categoria
 
 class DestinoForm(forms.ModelForm):
     class Meta:
         model = DestinosTuristicos
         fields = ['nombreCiudad', 'descripcionCiudad', 'imagenCiudad', 'precioTour', 'ofertaTour']
+
+class ComentarioForm(forms.ModelForm):
+    class Meta:
+        model = Comentario
+        fields = ['autor', 'contenido']
+
+class CategoriaForm(forms.ModelForm):
+    class Meta:
+        model = Categoria
+        field = ['nombre', 'destinos']

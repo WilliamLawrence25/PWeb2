d3ae40d5ea8f1f1b72f002e8fa538a82951c95cb
wchoquehuancab <wchoquehuancab@unsa.edu.pe>
2024-06-15 00:15:11 -0500

    Correciones de seguridad

--- Lab8/lab07/settings.py
+++ Lab8/lab07/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 from pathlib import Path
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +28,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 # Application definition
 
 INSTALLED_APPS = [
